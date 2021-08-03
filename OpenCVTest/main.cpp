9be24db8ea468b67f50c798d9daf44bfcc05a66a
yangyufan <yangyufan@xiaomi.com>
2021-08-03 16:29:46 +0800

    Fix: some format issue No bug

--- OpenCVTest/main.cpp
+++ OpenCVTest/main.cpp
@@ -1,19 +1,21 @@
-//
-//  main.cpp
-//  OpenCVTest
-//
-//  Created by 杨驭帆 on 2021/8/2.
-//
+/* ********************************************* *
+ * @Project: OpenCVTest
+ * @File:    main.cpp
+ *
+ * @Author:  yangyufan
+ * @Data:    2021/8/2.
+ * ********************************************* */
 
 #include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <fstream>
+
+#define BYTE unsigned char
+
 using namespace std;
 using namespace cv;
- 
-#define BYTE unsigned char
- 
+
 int main() {
   Mat image = imread("test.png");
 
